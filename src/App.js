0eceaaeff367cc36448d50508ecf479859939ae6
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-07-31 23:09:08 +0530

    Refactored Title Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import Title from './components/Title'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div className="title">
-        <h1>Firegram</h1>
-        <h2>Your Pictures</h2>
-        <p>ipsum dolor sit amet, consectetur adip</p>
-      </div>
+      <Title />
     </div>
   );
 }
---
+++ src/components/Title.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Title(props) {
+    return (
+        <div className="title">
+            <h1>Firegram</h1>
+            <h2>Your Pictures</h2>
+            <p>ipsum dolor sit amet, consectetur adip</p>
+        </div>
+    )
+}
+
+Title.propTypes = {
+
+}
+
+export default Title
+
