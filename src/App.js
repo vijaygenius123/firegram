2932dd700dbaed1614a2e9b1aa05a0560f97ab4e
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-07-31 23:06:26 +0530

    Added Title

--- src/App.js
+++ src/App.js
@@ -5,20 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title">
+        <h1>Firegram</h1>
+        <h2>Your Pictures</h2>
+        <p>ipsum dolor sit amet, consectetur adip</p>
+      </div>
     </div>
   );
 }
