e91c2ff8f7b0ee51f1af9f44ac9f4ade7b20e476
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-08-03 22:18:56 +0530

    UI Changes For ImageGrid

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -6,7 +6,7 @@ const ImageGrid = () => {
     const { docs } = useFirestore('images')
     console.log(docs)
     return (
-        <div className="image-grid">
+        <div className="img-grid">
             {docs && docs.map(doc => (
                 <div className="img-wrap" key={doc.id}>
                     <img src={doc.url} alt="" />
